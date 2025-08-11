df7b750172f66e2d3b09c397fe5d7ca081496166
NAO5802 <i100185b@gmail.com>
2025-08-11 16:18:52 +0900

    WIP: implement locate_graphic_protocol()

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,25 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     loop {}
 }
 
-use core::{panic::PanicInfo, slice};
+fn locate_graphic_protocol<'a>(
+    efi_system_table: &EfiSystemTable,
+) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+
+    let status = (efi_system_table.boot_services.locate_protocol)(
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut graphic_output_protocol as *mut *mut EfiGraphicsOutputProtocol as *mut *mut EfiVoid,
+    );
+
+    if status != EfiStatus::Success {
+        return Err("Failed to locate graphics output protocol");
+    }
+
+    Ok(unsafe { &*graphic_output_protocol })
+}
+
+use core::{panic::PanicInfo, ptr::null_mut, slice};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {

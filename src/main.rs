ae1f18ee36334ba6686b9f7b8de3c9f654e4b568
NAO5802 <i100185b@gmail.com>
2025-08-13 15:08:41 +0900

    WIP: implement signature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::asm, mem::offset_of, panic::PanicInfo, ptr::null_mut, slice};
+use core::{arch::asm, mem::offset_of, panic::PanicInfo, ptr::null_mut};
 
 type EfiVoid = u8;
 type EfiHandle = u64;
@@ -16,23 +16,86 @@ enum EfiStatus {
 
 #[unsafe(no_mangle)]
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    // TODO: rikai
+    let mut vram = init_vram(efi_system_table).expect("init_vram failed");
 
-    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
-
-    let vram = unsafe {
-        slice::from_raw_parts_mut(vram_addr as *mut u32, vram_byte_size / size_of::<u32>())
-    };
-
-    for e in vram {
-        *e = 0xffffff;
+    for y in 0..vram.height {
+        for x in 0..vram.width {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0x00ff00;
+            }
+        }
     }
 
     // println!("Hello, world!");
     loop {
         hlt();
     }
+}
+
+trait Bitmap {
+    fn bytes_per_pixel(&self) -> i64;
+    fn pixels_per_line(&self) -> i64;
+    fn width(&self) -> i64;
+    fn height(&self) -> i64;
+    fn buf_mut(&mut self) -> *mut u8;
+
+    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> *mut u32 {
+        todo!()
+    }
+
+    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
+        todo!()
+    }
+
+    fn is_in_x_range(&self, px: i64) -> bool {
+        todo!()
+    }
+
+    fn is_in_y_range(&self, py: i64) -> bool {
+        todo!()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct VramBufferInfo {
+    buf: *mut u8,
+    width: i64,
+    height: i64,
+    pixels_per_line: i64,
+}
+
+impl Bitmap for VramBufferInfo {
+    fn bytes_per_pixel(&self) -> i64 {
+        4
+    }
+
+    fn pixels_per_line(&self) -> i64 {
+        self.pixels_per_line
+    }
+
+    fn width(&self) -> i64 {
+        self.width
+    }
+
+    fn height(&self) -> i64 {
+        self.height
+    }
+
+    fn buf_mut(&mut self) -> *mut u8 {
+        self.buf
+    }
+}
+
+fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
+    let gp = locate_graphic_protocol(efi_system_table)?;
+
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
 }
 
 fn locate_graphic_protocol<'a>(
